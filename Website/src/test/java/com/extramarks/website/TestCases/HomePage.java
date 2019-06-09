bf0c0b4c1b016a4ebe88ec4f1c59d5db74776ebf
Thiru Patel <thirupatelqa@gmail.com>
2019-06-10 01:16:19 +0530

    Thiru Commit

---
+++ Website/src/test/java/com/extramarks/website/TestCases/HomePage.java
@@ -0,0 +1,18 @@
+package com.extramarks.website.TestCases;
+
+import com.extramarks.website.base.TestBase;
+import com.extramarks.website.utilities.*;
+
+public class HomePage extends TestBase{
+	ExcelOperations eo;
+	
+	String filepath = "C:\\Users\\THIRUPATHI\\git\\Extramarks\\Website\\src\\main\\java\\com\\extramarks\\website\\testdata\\credentials.xlsx";
+
+	public static void main(String args[]){
+		
+	
+	
+		
+	}
+
+}
